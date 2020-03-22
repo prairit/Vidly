32651cc970228dd1ac3266a2bdc5b8866f504f71
Prairit Verma <shivamverma64@gmail.com>
2020-03-23 01:04:31 +0530

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+
+        public DbSet<Movie> Employees { get; set; }
+    }
+}
