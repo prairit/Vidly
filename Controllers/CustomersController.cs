42fd494fe1bbd8a70879fc1646c1890a77e5a7e6
Prairit Verma <shivamverma64@gmail.com>
2020-03-25 19:26:13 +0530

    Added option to edit the customer with drop down list functionality

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,6 +23,29 @@ namespace Vidly.Controllers
             return View();
         }
 
+        public ActionResult New()
+        {
+            IEnumerable<MembershipType> membershipTypes= _context.MembershipTypes;
+            CustomerFormViewModel model = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm",model);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id==0)
+                _context.Customers.Add(customer);
+            else
+            {
+                _context.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+
         public ActionResult List()
         {
             IEnumerable<Customer> listCustomers = _context.Customers.Include(e =>e.MembershipType);
@@ -32,6 +56,22 @@ namespace Vidly.Controllers
         {
             IEnumerable<Customer> listCustomers = _context.Customers.Include(e=>e.MembershipType);
             return View(listCustomers.SingleOrDefault(e => e.Id == id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(e => e.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var model = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes
+            };
+
+            return View("CustomerForm", model);
         }
     }
 }
